e138883cbd8fe639381e57db0e0011db4d38d7c9
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2021-05-31 17:23:25 +0100

    qft.py: initial commit

---
+++ qft.py
@@ -0,0 +1,71 @@
+import argparse as ap
+import timeit as ti
+import qiskit as qk
+import math as m
+from qiskit import circuit
+from qiskit.compiler.transpiler import _transpile_circuit
+from qiskit.providers import aer
+from mpi4py import MPI
+
+
+def parse_args():
+  parser = ap.ArgumentParser(description='Read in the number of qubits.')
+  parser.add_argument('N_QUBITS',
+    type=int,
+    help='The number of qubits to be simulated. Must be in range 0 < N_QUBITS < 45.',
+    choices=range(1,45)
+  )
+
+  return parser.parse_args()
+
+def main():
+  run_start = ti.default_timer()
+
+  args = parse_args()
+
+  qft = qk.circuit.library.QFT(
+    num_qubits=args.N_QUBITS,
+    approximation_degree=0,
+    do_swaps=False,
+    inverse=False,
+    insert_barriers=False,
+  )
+  qft.save_state(label='statevector')
+
+  sim = aer.AerSimulator(method='statevector')
+
+  transp_qft = qk.transpile(qft, sim)
+
+  N_GATES = transp_qft.size()
+
+  COMM = MPI.COMM_WORLD
+  RANK = COMM.Get_rank()
+
+  if (RANK == 0):
+    print("Running QFT circuit")
+    print("  Number of qubits: ", args.N_QUBITS)
+    print("  Number of gates: ", N_GATES)
+    print()
+
+  qft_start = ti.default_timer()
+  result = sim.run(transp_qft, method='statevector', blocking_enable=True).result()
+  qft_stop = ti.default_timer()
+
+  result_dict=result.to_dict()
+
+  print("Result metadata:")
+  print(result_dict['metadata'])
+  print()
+
+  run_stop = ti.default_timer()
+
+  if RANK == 0:
+    print("Results:")
+    print('  Run time = {:g} s'.format(run_stop - run_start))
+    print('  QFT time = {:g} s'.format(qft_stop - qft_start))
+    print('  Measured probability of |0..0> state = {:g}'.format(abs(result.get_statevector()[0])**2))
+    print('  Calculated probability of |0..0> state = {:g}'.format(2**-args.N_QUBITS))
+
+
+if __name__ == "__main__":
+  main()
